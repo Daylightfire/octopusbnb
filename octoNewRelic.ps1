d3ec35309ecf14b8436bbcedb7cbb0dccc035194
Daylightfire <jojaroot@gmail.com>
2018-09-20 16:45:02 +0100

    Addeded Oct deploy vars

--- octoNewRelic.ps1
+++ octoNewRelic.ps1
@@ -1,15 +1,12 @@
 #Set octo required inputs
 
-#$apiKey = $OctopusParameters['ApiKey']
-#$user = $OctopusParameters['User']
-#$appId = $OctopusParameters['AppId']
-#$revision = $OctopusParameters['revision']
+$apiKey = $OctopusParameters['ApiKey']
+$user = $OctopusParameters['User']
+$appId = $OctopusParameters['AppId']
+$revision = $OctopusParameters['Octopus.Release.Number']
+$releaseNotes = $OctopusParameters['Octopus.Release.Notes']
 
 #Set Standard Vars
-$appId = 'xxxx'
-$APIKEY = 'xxxxxx'
-$revision = '0.0.0.0'
-$user = 'testy.test@test.com'
 $uri = "https://api.newrelic.com/v2/applications/$appId/deployments.json"
 $body = @{
     "deployment"= @{
